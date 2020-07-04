3978eca5ad2f2e011435e1df2134387698a77317
Xennis <code@xennis.org>
2020-07-04 19:57:41 +0200

    App | Fix long artist name overflows

--- apps/green_walking/lib/pages/detail.dart
+++ apps/green_walking/lib/pages/detail.dart
@@ -2,21 +2,25 @@ import 'dart:ui';
 
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:latlong/latlong.dart';
 import 'package:flutter/gestures.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
-import '../services/parks.dart';
 import '../types/place.dart';
 import '../widgets/place_list_tile.dart';
 
 class DetailPageArguments {
-  final LatLng coordinates;
+  final Place place;
 
-  DetailPageArguments(this.coordinates);
+  DetailPageArguments(this.place);
 }
 
 class DetailPage extends StatelessWidget {
+  final Place park;
+
+  DetailPage({Key key, @required this.park})
+      : assert(park != null),
+        super(key: key);
+
   // FIXME: Rework this and the functions below.
   Widget _description(String description) {
     if (description == null) {
@@ -45,10 +49,131 @@ class DetailPage extends StatelessWidget {
     return Image.network(image.url);
   }
 
-  Widget _imageAttribution(BuildContext context, PlaceImage image) {
-    if (image == null) {
-      return Row();
+  Widget _title(String name) {
+    if (name == null) {
+      return Text("Unbekannt");
     }
+    return Text(name);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          automaticallyImplyLeading: true,
+          title: _title(park.name),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => Navigator.pop(context, false),
+          )),
+      floatingActionButton: _DetailSpeedDial(park),
+      body: Column(
+        children: [
+          _image(park.image),
+          Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _location(park.location),
+                  CategoryChips(park.categories),
+                  // TODO: Add Wikipedia quote instead
+                  _description(park.description),
+                  Divider(),
+                  _DetailAttribution(park.wikidataId, park.image),
+                ],
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class _DetailAttribution extends StatelessWidget {
+  final String wikidataId;
+  final PlaceImage image;
+  final Color textColor = Colors.grey;
+
+  _DetailAttribution(this.wikidataId, this.image);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> children = [_wikidata(context)];
+    if (image != null) {
+      children.add(_imageAttribution(context, image));
+    }
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: children,
+    );
+  }
+
+  Widget _wikidata(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+                  title: Text('Data'),
+                  content: SingleChildScrollView(
+                    child: ListBody(
+                      children: <Widget>[
+                        RichText(
+                          text: TextSpan(
+                            text: 'Powered by Wikidata\n',
+                            style: TextStyle(color: Colors.blue),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                launch('https://www.wikidata.org');
+                              },
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            text: 'Improve this data',
+                            style: TextStyle(color: Colors.blue),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                launch(
+                                    'https://www.wikidata.org/wiki/${wikidataId}');
+                              },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  actions: [
+                    FlatButton(
+                        child: Text("OK"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        }),
+                  ],
+                ));
+      },
+      child: Wrap(
+        children: [
+          RichText(
+            text: TextSpan(style: TextStyle(color: textColor), children: [
+              TextSpan(
+                text: "Powered by Wikidata",
+              ),
+              TextSpan(text: " "),
+              WidgetSpan(
+                child: Icon(
+                  Icons.info,
+                  size: 15,
+                  color: textColor,
+                ),
+              ),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _imageAttribution(BuildContext context, PlaceImage image) {
     return GestureDetector(
       onTap: () {
         showDialog(
@@ -100,119 +225,22 @@ class DetailPage extends StatelessWidget {
                   ],
                 ));
       },
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Wrap(
         children: [
-          Row(
-            children: [
-              Text(
-                "Foto: ${image.artist} / ${image.licenseShortName}",
-                style: TextStyle(color: Colors.grey),
+          RichText(
+            text: TextSpan(style: TextStyle(color: textColor), children: [
+              TextSpan(
+                text: "Foto: ${image.artist} / ${image.licenseShortName}",
               ),
-              Icon(
+              TextSpan(text: " "),
+              WidgetSpan(
+                  child: Icon(
                 Icons.info,
-                size: 14,
-                color: Colors.grey,
-              )
-            ],
+                size: 15,
+                color: textColor,
+              ))
+            ]),
           ),
-        ],
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final DetailPageArguments args = ModalRoute.of(context).settings.arguments;
-    final Place park = ParkService.get(args.coordinates);
-
-    return Scaffold(
-      appBar: AppBar(
-          automaticallyImplyLeading: true,
-          title: Text(park.name),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () => Navigator.pop(context, false),
-          )),
-      floatingActionButton: _DetailSpeedDial(park),
-      body: Column(
-        children: [
-          _image(park.image),
-          Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _location(park.location),
-                  CategoryChips(park.categories),
-                  // TODO: Add Wikipedia quote instead
-                  _description(park.description),
-                  Divider(),
-                  GestureDetector(
-                    onTap: () {
-                      showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                                title: Text('Data'),
-                                content: SingleChildScrollView(
-                                  child: ListBody(
-                                    children: <Widget>[
-                                      RichText(
-                                        text: TextSpan(
-                                          text: 'Powered by Wikidata\n',
-                                          style: TextStyle(color: Colors.blue),
-                                          recognizer: TapGestureRecognizer()
-                                            ..onTap = () {
-                                              launch(
-                                                  'https://www.wikidata.org');
-                                            },
-                                        ),
-                                      ),
-                                      RichText(
-                                        text: TextSpan(
-                                          text: 'Improve this data',
-                                          style: TextStyle(color: Colors.blue),
-                                          recognizer: TapGestureRecognizer()
-                                            ..onTap = () {
-                                              launch(
-                                                  'https://www.wikidata.org/wiki/${park.wikidataId}');
-                                            },
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                actions: [
-                                  FlatButton(
-                                      child: Text("OK"),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      }),
-                                ],
-                              ));
-                    },
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              "Powered by Wikidata ",
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                            Icon(
-                              Icons.info,
-                              size: 14,
-                              color: Colors.grey,
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  _imageAttribution(context, park.image),
-                ],
-              )),
         ],
       ),
     );
