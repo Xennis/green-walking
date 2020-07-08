36a00a3422802c9945ff266d399af6f859c6c66a
Xennis <code@xennis.org>
2020-07-08 21:36:17 +0200

    Apps | Show park description

--- apps/green_walking/lib/types/place.dart
+++ apps/green_walking/lib/types/place.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/foundation.dart';
 import 'package:latlong/latlong.dart';
 
+class PlaceExtract {
+  final String text;
+  final String licenseShortName;
+  final String licenseUrl;
+
+  PlaceExtract({this.text, this.licenseShortName, this.licenseUrl});
+
+  factory PlaceExtract.fromJson(Map<dynamic, dynamic> j) {
+    return PlaceExtract(
+      text: j['text'],
+      licenseShortName: j['licenseShortName'],
+      licenseUrl: j['licenseUrl'],
+    );
+  }
+}
+
 class PlaceImage {
   final String artist;
   final String descriptionUrl;
@@ -32,6 +48,7 @@ class Place {
   final LatLng coordinateLocation;
   final String commonsUrl;
   final String description;
+  final PlaceExtract extract;
   final PlaceImage image;
   final String location;
   final String name;
@@ -45,6 +62,7 @@ class Place {
       this.coordinateLocation,
       this.commonsUrl,
       this.description,
+      this.extract,
       this.image,
       this.location,
       this.name,
@@ -75,6 +93,13 @@ class Place {
     if (j['image'] != null) {
       image = PlaceImage.fromJson(j['image']);
     }
+    PlaceExtract extract;
+    if (j['extract'] != null) {
+      var rawExtractLang = j['extract'][lang];
+      if (rawExtractLang != null) {
+        extract = PlaceExtract.fromJson(rawExtractLang);
+      }
+    }
 
     return Place(
         aliases: List<String>.from(j['aliases'][lang]),
@@ -82,6 +107,7 @@ class Place {
         coordinateLocation: coordinateLocation,
         commonsUrl: j['commonsUrl'],
         description: j['description'],
+        extract: extract,
         image: image,
         location: location,
         name: j['name'][lang],
