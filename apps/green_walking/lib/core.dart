872fe945de14b2b1bd9c78cfdd810c0dbf8614cc
Xennis <code@xennis.org>
2020-07-17 22:47:09 +0200

    Apps | Add tests for truncate function

--- apps/green_walking/lib/core.dart
+++ apps/green_walking/lib/core.dart
@@ -1,4 +1,10 @@
 String truncateString(String myString, int cutoff) {
+  if (myString == null) {
+    return null;
+  }
+  if (cutoff == null || cutoff < 1) {
+    return null;
+  }
   return (myString.length <= cutoff)
       ? myString
       : '${myString.substring(0, cutoff)}...';
