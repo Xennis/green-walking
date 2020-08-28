2a04febf9911710dcaf19bb8a8c6b84a4892c5cb
Xennis <code@xennis.org>
2020-08-28 21:37:08 +0200

    Apps | web | Fix SVG

---
+++ apps/green_walking/lib/src/svg.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+// flutter_svg does not support web yet. See https://github.com/dnfield/flutter_svg/issues/173
+class PlatformSvgPicture {
+  static Widget asset(String assetName,
+      {double width,
+      double height,
+      BoxFit fit = BoxFit.contain,
+      Color color,
+      Alignment alignment = Alignment.center,
+      String semanticsLabel}) {
+    if (kIsWeb) {
+      return Image.network('/assets/$assetName',
+          width: width,
+          height: height,
+          fit: fit,
+          color: color,
+          alignment: alignment,
+          semanticLabel: semanticsLabel);
+    }
+    return SvgPicture.asset(assetName,
+        width: width,
+        height: height,
+        fit: fit,
+        color: color,
+        alignment: alignment,
+        semanticsLabel: semanticsLabel);
+  }
+}
