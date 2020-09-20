c5daaa5d12fbbf4e7f6d4fbfc1f9f85b2992c085
Xennis <code@xennis.org>
2020-09-20 22:25:05 +0200

    Apps | Use custom outdoor style

--- apps/green_walking/lib/src/mapbox.dart
+++ apps/green_walking/lib/src/mapbox.dart
@@ -1,14 +1,12 @@
-enum MabboxTileset { outdoor, satellite, streets }
+enum MabboxTileset { outdoor, satellite }
 
 extension MabboxTilesetExtension on MabboxTileset {
   String get id {
     switch (this) {
       case MabboxTileset.outdoor:
-        return 'outdoors-v11';
+        return 'xennis/ckfbioyul1iln1ap0pm5hrcgy';
       case MabboxTileset.satellite:
-        return 'satellite-streets-v11';
-      case MabboxTileset.streets:
-        return 'streets-v11';
+        return 'mapbox/satellite-streets-v11';
       default:
         return null;
     }
