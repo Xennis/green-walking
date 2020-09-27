f27f4ca5aafea8a28709bdd62dd224380fbeaa7b
Xennis <code@xennis.org>
2020-09-27 16:13:36 +0200

    Apps | Add test for search page

---
+++ apps/green_walking/test/pages/search_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:green_walking/intl.dart';
+import 'package:green_walking/pages/search.dart';
+import 'package:green_walking/src/mapbox/geocoding.dart';
+
+void main() {
+  testWidgets('SearchPage() can be rendered', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+        home: SearchPage(
+            result:
+                Future<MapboxGeocodingResult>.value(MapboxGeocodingResult())),
+        localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+          AppLocalizationsDelegate()
+        ]));
+  });
+}
