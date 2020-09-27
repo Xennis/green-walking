7c06f9a988e962730bff0bf98929abb4612095fa
Xennis <code@xennis.org>
2020-09-27 20:38:26 +0200

    Pipelines | Re-work Wikidata query transform to support more countries

--- pipelines/places/greenwalking/pipeline/places/wikidata/query_test.py
+++ pipelines/places/greenwalking/pipeline/places/wikidata/query_test.py
@@ -2,8 +2,10 @@ import os
 import tempfile
 import unittest
 
+from apache_beam import Create
 from apache_beam.testing.test_pipeline import TestPipeline
 from apache_beam.testing.util import assert_that, equal_to
+from sqlitedict import SqliteDict
 
 from greenwalking.pipeline.places.wikidata import Query
 
@@ -11,17 +13,14 @@ from greenwalking.pipeline.places.wikidata import Query
 class TestQuery(unittest.TestCase):
     def test_known(self):
         with tempfile.TemporaryDirectory() as base_path:
-            state_file = os.path.join(base_path, "state.json")
-            with open(state_file, "w") as f:
-                f.write("Q1234\nQ54321")
-            expected = ["Q1234", "Q54321"]
+            cache_file = os.path.join(base_path, "cache.json")
+            cache = SqliteDict(cache_file, autocommit=True)
+            cache["monument"] = ["Q1234", "Q54321"]
+            cache.close()
+
+            data = [("monument", "query-does-not-matter")]
+            expected = [("Q1234", "monument"), ("Q54321", "monument")]
 
             with TestPipeline() as p:
-                actual = p | Query("does not matter", state_file=state_file, user_agent="some-agent")
+                actual = p | Create(data) | Query(cache_file, user_agent="some-agent")
                 assert_that(actual, equal_to(expected))
-
-            actual_state = []
-            with open(state_file) as f:
-                for line in f:
-                    actual_state.append(line.rstrip("\n"))
-            self.assertCountEqual(actual_state, expected)
