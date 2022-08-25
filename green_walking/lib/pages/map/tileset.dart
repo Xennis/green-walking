1321c3de9a4626fa62f439a2f242f464ab7d1ca9
Xennis <code@xennis.org>
2022-08-25 19:05:52 +0200

    Add vector and static layer

--- green_walking/lib/pages/map/tileset.dart
+++ green_walking/lib/pages/map/tileset.dart
@@ -11,4 +11,15 @@ extension MabboxTilesetExtension on MabboxTileset {
         return null;
     }
   }
+
+  String? get stylePath {
+    switch (this) {
+      case MabboxTileset.outdoor:
+        return 'mapbox://styles/xennis/ckfbioyul1iln1ap0pm5hrcgy';
+      case MabboxTileset.satellite:
+        return 'mapbox://styles/xennis/ckfc5mxh33tjg19qvk6m5f5hj';
+      default:
+        return null;
+    }
+  }
 }
