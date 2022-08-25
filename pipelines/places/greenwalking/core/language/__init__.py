037fa989bb2b7f8c26db69a497c02a7152de5c0e
Xennis <code@xennis.org>
2022-08-25 17:14:45 +0200

    Delete pipelines

--- pipelines/places/greenwalking/core/language/__init__.py
+++
@@ -1,7 +0,0 @@
-from typing import NewType
-
-Language = NewType("Language", str)
-
-# ISO 639-1 codes (https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes)
-ENGLISH = Language("en")
-GERMAN = Language("de")
--- pipelines/places/greenwalking/pipeline/places/__init__.py
+++
@@ -1,1 +0,0 @@
-from .places import run
