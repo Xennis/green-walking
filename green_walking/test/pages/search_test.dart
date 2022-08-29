af673b6758748bbb34749fac534dd6f3d6e1fb84
Xennis <code@xennis.org>
2022-08-29 17:49:52 +0200

    [Fix] Due to search box keyboard always pops up

--- green_walking/test/pages/search_test.dart
+++ green_walking/test/pages/search_test.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:green_walking/pages/search.dart';
-import 'package:green_walking/services/mapbox_geocoding.dart';
 
 void main() {
   testWidgets('SearchPage() can be rendered', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-        home: SearchPage(Future<MapboxGeocodingResult>.value(
-            MapboxGeocodingResult(<MaboxGeocodingPlace>[]))),
-        localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+    await tester.pumpWidget(const MaterialApp(
+        home: SearchPage(
+          accessToken: 'some-token',
+        ),
+        localizationsDelegates: <LocalizationsDelegate<dynamic>>[
           AppLocalizations.delegate
         ]));
   });
