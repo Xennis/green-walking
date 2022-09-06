98b35b837c87424a82b29692981756c697e353c3
Xennis <code@xennis.org>
2022-09-06 22:01:08 +0200

    [Fix] Last position is not shown during start

--- green_walking/lib/services/shared_prefs.dart
+++ green_walking/lib/services/shared_prefs.dart
@@ -1,23 +1,31 @@
 import 'dart:convert';
 import 'dart:developer';
 
-import 'package:mapbox_gl/mapbox_gl.dart' show LatLng;
+import 'package:mapbox_gl/mapbox_gl.dart' show CameraPosition, LatLng;
 import 'package:shared_preferences/shared_preferences.dart';
 
 // ignore: avoid_classes_with_only_static_members
 class SharedPrefs {
-  static const String keyLastLocation = 'last-location';
+  static const String keyLastPosition = 'last-position';
   static const String analyticsEnabled = 'analytics-enabled';
 
-  static Future<LatLng?> getLatLng(String key) async {
+  static Future<CameraPosition?> getCameraPosition(String key) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     final String? raw = prefs.getString(key);
     if (raw == null) {
       return null;
     }
     try {
-      final List<double> parsed = jsonDecode(raw) as List<double>;
-      return LatLng(parsed[0], parsed[1]);
+      final Map<String, dynamic> parsed =
+          jsonDecode(raw) as Map<String, dynamic>;
+      final List<dynamic> parsedTarget = parsed['target'] as List<dynamic>;
+
+      return CameraPosition(
+        bearing: parsed['bearing'],
+        target: LatLng(parsedTarget[0] as double, parsedTarget[1] as double),
+        tilt: parsed['tilt'],
+        zoom: parsed['zoom'],
+      );
     } catch (e) {
       // No last location is not critical. A raised exception on start up is.
       log('failed to load location: $e');
@@ -25,9 +33,9 @@ class SharedPrefs {
     }
   }
 
-  static void setLatLng(String key, LatLng val) {
+  static void setCameraPosition(String key, CameraPosition val) {
     try {
-      final String raw = val.toJson().toString();
+      final String raw = jsonEncode(val.toMap());
       SharedPreferences.getInstance()
           .then((SharedPreferences prefs) => prefs.setString(key, raw));
     } catch (e) {
